ff64b548474dcb79e6238c4d7911cdf0fb8df0dc
aczarnowski <adriannnczarnowski@gmail.com>
2022-03-22 10:41:38 +0100

    fix after Mateusz feedback

--- src/main/java/models/Actor.java
+++ src/main/java/models/Actor.java
@@ -1,33 +1,6 @@
 package models;
 
-public class Actor {
-
-    private String firstName;
-    private String lastName;
-
-    public Actor() {
-    }
-
-    public Actor(String firstName, String lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+public class Actor extends Person {
 
     @Override
     public String toString() {
--- src/main/java/models/Director.java
+++ src/main/java/models/Director.java
@@ -1,32 +1,6 @@
 package models;
 
-public class Director {
-    private String firstName;
-    private String lastName;
-
-    public Director() {
-    }
-
-    public Director(String firstName, String lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+public class Director extends Person {
 
     @Override
     public String toString() {
